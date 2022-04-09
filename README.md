588977f5dfe56ec47f138773148e8a55207af4b0
princessxj9 <johnsonprincess25@gmail.com>
2022-04-09 09:29:29 -0400

    commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# Web Game IV
-
-This is an activity for FSWD Lesson 5.3.6- Callback Functions
-
-Please refer to the Activity Guide in Canvas for directions.
--- move.js
+++ move.js
@@ -7,6 +7,60 @@ function move(element) {
     }
 
     return {
-        to: moveToCoordinates
-    }
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }        
 }
+function moveWithArrowKeys(left, bottom, callBack){
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    element.style.left = x + 'px'
+    element.style.bottom = y + 'px'
+    
+    function moveCharacter(){ 
+        if(direction === 'west'){
+            x-=1
+        }
+        if(direction === 'north'){
+            y+=1
+        }
+        if(direction === 'east'){
+            x+=1
+        }
+        if(direction === 'south'){
+            y-=1
+        }
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+    }
+    
+    setInterval(moveCharacter, 1)
+    //refactor our code into the move function. will allow us to use it on any image.//
+    document.addEventListener('keydown', function(e){
+        if(e.repeat) return;
+    
+        if(e.key === 'ArrowLeft'){
+            direction = 'west'
+        }
+        if(e.key === 'ArrowUp'){
+            direction = 'north'
+        }
+        if(e.key === 'ArrowRight'){
+            direction = 'east'
+        }
+        if(e.key === 'ArrowDown'){
+            direction = 'south'
+        }
+        callBack()
+    })
+    
+  //event listener to stop the character when the user releases a key//
+    document.addEventListener('keyup', function(e){
+        direction = null
+        callBack()
+    })
+    
+}
+
