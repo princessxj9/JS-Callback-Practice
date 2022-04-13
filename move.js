3ecdd3deb35c45283f35bb977ae7c2ae6f976ca6
princessxj9 <johnsonprincess25@gmail.com>
2022-04-13 15:42:09 -0400

    commit

--- move.js
+++ move.js
@@ -1,72 +1,3 @@
-/*function move(element) {
-    element.style.position = 'fixed'
-
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
-    
-     
-}
-function moveWithArrowKeys(left, bottom, callBack){
-    let direction = null;
-    let x = left;
-    let y = bottom;
-
-    element.style.left = x + 'px'
-    element.style.bottom = y + 'px'
-    
-    function moveCharacter(){ 
-        if(direction === 'west'){
-            x-=1
-        }
-        if(direction === 'north'){
-            y+=1
-        }
-        if(direction === 'east'){
-            x+=1
-        }
-        if(direction === 'south'){
-            y-=1
-        }
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
-    }
-    
-    setInterval(moveCharacter, 1)
-    //refactor our code into the move function. will allow us to use it on any image.//
-    document.addEventListener('keydown', function(e){
-        if(e.repeat) return;
-    
-        if(e.key === 'ArrowLeft'){
-            direction = 'west'
-        }
-        if(e.key === 'ArrowUp'){
-            direction = 'north'
-        }
-        if(e.key === 'ArrowRight'){
-            direction = 'east'
-        }
-        if(e.key === 'ArrowDown'){
-            direction = 'south'
-        }
-        callBack()
-    })
-    
-  //event listener to stop the character when the user releases a key//
-    document.addEventListener('keyup', function(e){
-        direction = null
-        callBack(direction)
-    })
-    {
-    
-}
-return {
-    to: moveToCoordinates,
-    withArrowKeys: moveWithArrowKeys
-}   
-
-}}*/
-
 function move(element) {
     element.style.position = 'fixed'
 
@@ -101,6 +32,8 @@ function move(element) {
         }
         
         setInterval(moveCharacter, 1)
+      
+        //refactor our code into the move function. will allow us to use it on any image.//
         
         document.addEventListener('keydown', function(e){
             if(e.repeat) return;
@@ -119,7 +52,9 @@ function move(element) {
             }
             callback(direction)
         })
-        
+       
+        //event listener to stop the character when the user releases a key//
+
         document.addEventListener('keyup', function(e){
             direction = null
             callback(direction)
