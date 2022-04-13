221ac9c36ed50b0024045a5942970bbeed3797b0
princessxj9 <johnsonprincess25@gmail.com>
2022-04-13 14:54:01 -0400

    commit

--- move.js
+++ move.js
@@ -1,10 +1,10 @@
-function move(element) {
+/*function move(element) {
     element.style.position = 'fixed'
 
     function moveToCoordinates(left, bottom) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
-    }
+    
      
 }
 function moveWithArrowKeys(left, bottom, callBack){
@@ -57,6 +57,7 @@ function moveWithArrowKeys(left, bottom, callBack){
         direction = null
         callBack(direction)
     })
+    {
     
 }
 return {
@@ -64,3 +65,69 @@ return {
     withArrowKeys: moveWithArrowKeys
 }   
 
+}}*/
+
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+        function moveCharacter(){ 
+            if(direction === 'west'){
+                x-=1
+            }
+            if(direction === 'north'){
+                y+=1
+            }
+            if(direction === 'east'){
+                x+=1
+            }
+            if(direction === 'south'){
+                y-=1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            callback(direction)
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            callback(direction)
+        })
+    }
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }
+}
